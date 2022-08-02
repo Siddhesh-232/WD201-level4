97ee9094c4d0b567abaf76b4654c3937a0b57cb4
Sidd <dandekarsiddhesh.02@gmail.com>
2022-08-02 22:46:47 +0530

    Corrected

--- todo.rb
+++ todo.rb
@@ -1,40 +1,52 @@
 require "date"
-require "record"
+require "active_record"
+
 class Todo < ActiveRecord::Base
-  def due_today?
-    due_date == Date.today
-  end
-  def due_later?
-    due_date > Date.today
-  end
   def overdue?
     due_date < Date.today
   end
-  def self.due_today
-    where("due_date = ?", Date.today)
+
+  def due_today?
+    due_date == Date.today
   end
-  def self.due_later
-    where("due_date > ?", Date.today)
+
+  def due_later?
+    due_date > Date.today
   end
+
   def self.overdue
     where("due_date < ?", Date.today)
   end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
   def to_displayable_string
     status = completed ? "[x]" : "[ ]"
     date = due_today? ? nil : due_date
     "#{id}. #{status} #{todo_text} #{date}"
   end
+
   def self.show_list
     puts "My Todo-list\n\n"
+
+    puts "Overdue"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
     puts "Due Today"
     puts due_today.map { |todo| todo.to_displayable_string }
     puts "\n\n"
+
     puts "Due Later"
     puts due_later.map { |todo| todo.to_displayable_string }
   end
-  puts "Overdue"
-    puts overdue.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
+
   def self.add_task(task)
     create!(
       todo_text: task[:todo_text],
@@ -42,10 +54,12 @@ class Todo < ActiveRecord::Base
       completed: false,
     )
   end
+
   def self.mark_as_complete(id)
     todo = find(id)
     todo.completed = true
     todo.save!
+
     puts todo.to_displayable_string
   end
 end
